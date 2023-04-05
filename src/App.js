00ad05151181884d8bd23c305a6b49a5886422de
graftcode <graftcode@outlook.com>
2023-04-05 17:38:47 +0100

    msw play about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+
+import MovieIcon from "./assets/MovieIcon";
+
+import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [moviesData, setMoviesData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numberOfPages, setNumberOfPages] = useState(1);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setMoviesData([]);
+    if (pageNumber !== 1) setPageNumber(1);
+
+    if (!searchTerm) return;
+
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?apikey=a93ceeb4&s=${searchTerm}&page=${pageNumber}`
+    );
+
+    setMoviesData(data?.Search);
+    setNumberOfPages(Math.ceil(Number(data?.totalResults) / 10));
+  };
+
+  const handleLoadMore = async () => {
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?apikey=a93ceeb4&s=${searchTerm}&page=${
+        pageNumber + 1
+      }`
+    );
+
+    if (data) {
+      setPageNumber(pageNumber + 1);
+      setMoviesData([...moviesData, ...data?.Search]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="container">
+        <h1>Napo Movies</h1>
+
+        <form className="search-movies-form" onSubmit={handleSubmit}>
+          <button className="search-movies-form-button">🔍</button>
+          <input
+            type="text"
+            placeholder="Search for a movie..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+
+        <div className="search-movies-result">
+          {moviesData.length > 0 &&
+            moviesData.map((movie) => (
+              <div className="movie-item" key={movie.imdbID}>
+                {process.env.NODE_ENV === "development" ? (
+                  <MovieIcon />
+                ) : (
+                  <img src={movie.Poster} alt={movie.Title} />
+                )}
+                <h3>{movie.Title}</h3>
+              </div>
+            ))}
+        </div>
+        {pageNumber !== numberOfPages && (
+          <button className="load-more-button" onClick={handleLoadMore}>
+            Load More
+          </button>
+        )}
+      </div>
+    </main>
   );
 }
 
